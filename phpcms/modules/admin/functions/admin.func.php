0f15d3d813e31d50cc572a433b9679337ea75fa8
dongxw <wudy.dong@yahoo.com>
2016-02-21 15:24:48 +0800

    cancel background remote notice cancel homepage "up video" function

--- phpcms/modules/admin/functions/admin.func.php
+++ phpcms/modules/admin/functions/admin.func.php
@@ -27,8 +27,8 @@
 	}
 
 	function system_information($data) {
-		$update = pc_base::load_sys_class('update');
-		$notice_url = $update->notice();
+		//$update = pc_base::load_sys_class('update');
+		//$notice_url = $update->notice();
 		$string = base64_decode('PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPiQoIiNtYWluX2ZyYW1laWQiKS5yZW1vdmVDbGFzcygiZGlzcGxheSIpOzwvc2NyaXB0PjxkaXYgaWQ9InBocGNtc19ub3RpY2UiPjwvZGl2PjxzY3JpcHQgdHlwZT0idGV4dC9qYXZhc2NyaXB0IiBzcmM9Ik5PVElDRV9VUkwiPjwvc2NyaXB0Pg==');
 		echo $data.str_replace('NOTICE_URL',$notice_url,$string);
 	}
